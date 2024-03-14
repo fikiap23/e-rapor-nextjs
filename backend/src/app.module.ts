5894cc00144401610bea40c6b0d11b2eb411b2c9
fikiaprian23 <fikiaprian23@gmail.com>
2024-03-15 05:58:35 +0700

    refactor and add backend nestjs

---
+++ backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { AuthModule } from './auth/auth.module';
+import { SubjectModule } from './subject/subject.module';
+
+@Module({
+  imports: [AuthModule, SubjectModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
