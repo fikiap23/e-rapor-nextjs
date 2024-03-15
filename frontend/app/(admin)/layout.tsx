518ab95ef1bb6919e4e3c6676695c5aa357bc7a1
fikiaprian23 <fikiaprian23@gmail.com>
2024-03-15 07:57:35 +0700

    refactor folder

--- frontend/app/(admin)/layout.tsx
+++ frontend/app/(admin)/layout.tsx
@@ -1,6 +1,6 @@
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import Sidebar from '@/components/Sidebar'
+import Footer from '@/components/shared/Footer'
+import Header from '@/components/shared/Header'
+import Sidebar from '@/components/ui/admin/Sidebar'
 import React from 'react'
 const AboutLayout = ({ children }: { children: React.ReactNode }) => {
   return (
