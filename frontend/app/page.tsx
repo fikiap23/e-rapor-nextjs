ecc0a753c9dd607a9db554193458bc430f3b260e
fr0faive <faikar.moch.tajudin@gmail.com>
2024-03-19 17:41:07 +0700

    prepare login page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
 export default function Home() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Link href={"/auth/login"}>Login</Link>
+    </div>
+  );
 }
--- frontend/app/teacher/layout.tsx
+++ frontend/app/teacher/layout.tsx
@@ -1,17 +1,17 @@
-import Footer from '@/components/shared/Footer'
-import Header from '@/components/shared/Header'
-import SidebarTeacher from '@/components/shared/SidebarTeacher'
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+import SidebarTeacher from "@/components/shared/SidebarTeacher";
 
-import React from 'react'
+import React from "react";
 const AboutLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Header></Header>
-      <SidebarTeacher></SidebarTeacher>
+      <Header />
+      <SidebarTeacher />
       <div>{children}</div>
-      <Footer></Footer>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default AboutLayout
+export default AboutLayout;
