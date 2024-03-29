48342cceae5a2ec717cc2e776770fdd22463ba93
fr0faive <faikar.moch.tajudin@gmail.com>
2024-03-29 10:46:59 +0700

    lupa

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+"use client";
+import { login } from "@/services/authServices";
+import React, { useState } from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [data, setData] = useState({});
+  const handleChange = (event: any) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(data);
+    try {
+      login(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="w-full h-full bg-blue-600">
+      <div className="flex justify-center items-center h-full">
+        <section className=" bg-white rounded-3xl p-10">
+          <h1 className="font-bold text-center">Login</h1>
+          <form
+            action=""
+            className="flex flex-col gap-3"
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="text"
+              name="username"
+              placeholder="Username atau Email"
+              className="p-3"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="p-3"
+              onChange={handleChange}
+            />
+            <button type="submit" className="bg-blue-500 text-white p-3">
+              Login
+            </button>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
