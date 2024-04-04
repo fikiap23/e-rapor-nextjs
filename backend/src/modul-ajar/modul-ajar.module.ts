e36ae4e191c396f97d49eb9136fb87b17f25b304
fikiaprian23 <fikiaprian23@gmail.com>
2024-04-04 19:49:43 +0700

    add create, update modul ajar

--- backend/src/modul-ajar/modul-ajar.module.ts
+++ backend/src/modul-ajar/modul-ajar.module.ts
@@ -9,6 +9,7 @@ import { ModulAjarRepository } from './modul-ajar.repository';
 import { ModulAjarQuery } from '../prisma/queries/modul-ajar/modul-ajar.query';
 import { AuthModule } from '../auth/auth.module';
 import { ModulAjarController } from './modul-ajar.comtroller';
+import { CpTpModule } from '../cp-tp/cp-tp.module';
 
 
 @Module({
@@ -19,6 +20,7 @@ import { ModulAjarController } from './modul-ajar.comtroller';
         ConfigModule,
         HelperModule,
         AuthModule,
+        CpTpModule
     ],
     providers: [ModulAjarService, ModulAjarRepository, ModulAjarQuery],
     controllers: [ModulAjarController],
