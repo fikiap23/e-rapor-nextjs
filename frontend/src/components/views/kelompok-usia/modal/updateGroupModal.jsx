6b9ace56fec11537331b102cc656570703fd87f2
fikiaprian23 <fikiaprian23@gmail.com>
2024-04-07 07:55:26 +0700

    fix update kelompok usia

--- frontend/src/components/views/kelompok-usia/modal/updateGroupModal.jsx
+++ frontend/src/components/views/kelompok-usia/modal/updateGroupModal.jsx
@@ -1,3 +1,5 @@
+import { Modal, Form, Input, Button } from 'antd'
+import useAuth from '@/hooks/useAuth'
 import rombelService from '@/services/rombel.service'
 import { useEffect, useState } from 'react'
 import Swal from 'sweetalert2'
@@ -9,123 +11,91 @@ const UpdateGroupModal = ({
   selectedKategori,
   token,
 }) => {
+  const [form] = Form.useForm()
   const [isLoading, setIsLoading] = useState(false)
-  const [formValues, setFormValues] = useState({
-    name: '',
-    kelompokUsia: '',
-    kode: '',
-  })
-  useEffect(() => {
-    if (selectedKategori) {
-      setFormValues({
-        name: selectedKategori.name,
-        kelompokUsia: selectedKategori.kelompokUsia,
-        kode: selectedKategori.kode,
-      })
-    }
-  }, [selectedKategori])
+  const { name, kelompokUsia, kode } = selectedKategori || {}
 
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setFormValues({ ...formValues, [name]: value })
-  }
+  useEffect(() => {
+    form.setFieldsValue({ name, kelompokUsia, kode })
+  }, [selectedKategori])
 
   const handleSubmit = async () => {
     try {
       setIsLoading(true)
-      // Tambahkan logika untuk menyimpan data kelas
-      const result = await rombelService.updateKategori(
-        token,
-        selectedKategori.id,
-        formValues
-      )
-      if (result.message == 'OK') {
-        refetch()
-        Swal.fire({
-          icon: 'success',
-          title: 'Berhasil!',
-          text: 'Data kelompok usia rombel telah diperbarui',
+      const values = await form.validateFields()
+      await rombelService
+        .updateKategori(token, selectedKategori.id, values)
+        .then((result) => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Berhasil!',
+            text: 'Data kelompok usia rombel telah diperbarui',
+          })
+          refetch()
+          closeModal()
+          setIsLoading(false)
         })
-        closeModal()
-      }
-
-      setIsLoading(false)
+        .catch((error) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Terjadi kesalahan: ' + error,
+          })
+          setIsLoading(false)
+        })
     } catch (error) {
       setIsLoading(false)
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
-        text: error,
+        text: 'Terjadi kesalahan',
       })
     }
   }
 
   return (
-    <div
-      className={`modal fade ${isOpen ? 'in show-modal' : ''}`}
-      id="update-modal"
+    <Modal
+      visible={isOpen}
+      title="Perbarui Data Kategori Rombel"
+      onCancel={closeModal}
+      footer={[
+        <Button key="back" onClick={closeModal}>
+          Batal
+        </Button>,
+        <Button
+          key="submit"
+          type="primary"
+          loading={isLoading}
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>,
+      ]}
     >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <button
-              type="button"
-              className="close"
-              data-dismiss="modal"
-              aria-label="Close"
-              onClick={closeModal}
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-            <h4 className="modal-title">Perbarui Data Kategori Rombel</h4>
-          </div>
-          <div className="modal-body">
-            <div>
-              <div className="box-body">
-                <div className="form-group">
-                  <label>Nama Kategori Rombel</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="name"
-                    value={formValues.name}
-                    required
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Kelompok Usia</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="kelompokUsia"
-                    value={formValues.kelompokUsia}
-                    required
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Kode</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="kode"
-                    value={formValues.kode}
-                    required
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
-              <div className="box-footer">
-                <button onClick={handleSubmit} className="btn btn-primary">
-                  {isLoading ? 'Loading...' : 'Submit'}
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Form form={form} layout="vertical">
+        <Form.Item
+          label="Nama Kategori Rombel"
+          name="name"
+          rules={[{ required: true, message: 'Masukkan Nama' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Kelompok Usia"
+          name="kelompokUsia"
+          rules={[{ required: true, message: 'Masukkan Kelompok Usia' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Kode"
+          name="kode"
+          rules={[{ required: true, message: 'Masukkan Kode' }]}
+        >
+          <Input />
+        </Form.Item>
+      </Form>
+    </Modal>
   )
 }
 
