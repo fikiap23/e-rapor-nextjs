bc8ca8f528c2292ab6f72a16055bdae22ff1eea7
mkp-kemal <kemal.garut7@gmail.com>
2024-04-11 07:50:33 +0700

    add search, filter and fix logic

--- frontend/src/components/views/rombel-diampu/tableLecture.jsx
+++ frontend/src/components/views/rombel-diampu/tableLecture.jsx
@@ -1,31 +1,39 @@
 'use client'
-import { Button, Table, Tag, Modal, Input } from 'antd'
+import { Button, Table, Tag, Input, Space } from 'antd';
+import { useState } from 'react';
 
 const TableLecture = ({ rombels, fetching }) => {
+  const [searchKeyword, setSearchKeyword] = useState('');
+
   const columns = [
     {
       title: 'No',
       dataIndex: 'index',
       key: 'index',
       render: (text, record, index) => index + 1,
+      sorter: (a, b) => a.index - b.index,
     },
     {
       title: 'Kelompok Usia',
       dataIndex: 'kelompokUsia',
       key: 'kelompokUsia',
-      // sorter: (a, b) => a.nip.localeCompare(b.nip),
+      filteredValue: [searchKeyword],
+      onFilter: (value, record) => {
+        return String(record.semester).toLowerCase().includes(value.toLowerCase());
+      },
+      sorter: (a, b) => a.kelompokUsia.localeCompare(b.kelompokUsia),
     },
     {
       title: 'Rombel',
       dataIndex: 'name',
       key: 'name',
-      // sorter: (a, b) => a.nama.localeCompare(b.nama),
+      sorter: (a, b) => a.name.localeCompare(b.name),
     },
     {
       title: 'Tahun Ajaran',
       dataIndex: 'semester',
       key: 'semester',
-      // sorter: (a, b) => a.nama.localeCompare(b.nama),
+      sorter: (a, b) => a.semester.localeCompare(b.semester),
     },
     {
       title: 'Status',
@@ -41,29 +49,53 @@ const TableLecture = ({ rombels, fetching }) => {
       title: 'Aksi',
       key: 'action',
       render: (text, record) => (
-        <span>
+        <Space>
           <Button
             type="primary"
             onClick={() => {
               const url = `/guru/rombel/${record.id}`;
               window.location.href = url;
             }}
-            style={{ marginRight: 8 }}
           >
-            <i className="fa fa-plus" style={{ marginRight: '8px' }}></i> Input Catatan Raport
+            <i className="fa fa-eye" style={{ marginRight: '8px' }}></i> Lihat Daftar Siswa
           </Button>
-        </span>
+        </Space>
       ),
     },
-  ]
-  return (
-    <Table
-      columns={columns}
-      dataSource={rombels}
-      loading={fetching}
-      scroll={{ x: 1000 }}
-    />
-  )
-}
+  ];
 
-export default TableLecture
+  const handleSearch = (value) => {
+    setSearchKeyword(value);
+  };
+
+  const handleChangeSearch = (e) => {
+    setSearchKeyword(e.target.value)
+  }
+
+  // // Filter data berdasarkan nilai pencarian
+  // const filteredRombels = rombels.filter((rombel) =>
+  //   rombel.name.toLowerCase().includes(searchKeyword.toLowerCase())
+  // );
+
+  return (
+    <>
+      <div style={{width: '30%'}}>
+        <Input.Search
+          placeholder="Cari data..."
+          onSearch={handleSearch}
+          onChange={handleChangeSearch}
+          style={{ marginBottom: 16 }}
+        />
+      </div>
+
+      <Table
+        columns={columns}
+        dataSource={rombels}
+        loading={fetching}
+        scroll={{ x: 1000 }}
+      />
+    </>
+  );
+};
+
+export default TableLecture;
