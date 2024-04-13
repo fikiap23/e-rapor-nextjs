2ac5befe02f38066f06dd270baecd4ddfdbd4bb3
fikiaprian23 <fikiaprian23@gmail.com>
2024-04-13 08:26:01 +0700

    add aksi

--- frontend/src/components/views/penilaian/table/muridTable.jsx
+++ frontend/src/components/views/penilaian/table/muridTable.jsx
@@ -1,56 +1,26 @@
 import React from 'react'
-import { Button, Table } from 'antd'
+import { Button, Table, Dropdown, Menu } from 'antd'
+import { MoreOutlined } from '@ant-design/icons'
 import { useMuridWithPenilaian } from '@/hooks/useMuridWithPenilaian'
 
 const MuridTable = ({ idRombelSemesterGuru, tp }) => {
-  // console.log(
-  //   `This from murid table with idRombelSemesterGuru ${idRombelSemesterGuru}`
-  // )
-  // console.log(`This from murid table with tp ${tp.id}`)
-  // console.log(`This from murid table with tp ${tp.minggu}`)
-  // console.log(
-  //   `This from murid table with tp ${tp.tujuanPembelajaranAgamaBudipekerti}`
-  // )
   const { data: muridWithPenilaian, isFetching: isFetchingMuridWithPenilaian } =
     useMuridWithPenilaian(idRombelSemesterGuru, tp.id)
-  console.log(muridWithPenilaian)
-  const dummyData = [
-    {
-      id: 1,
-      nis: '1234567890',
-      nama: 'John Doe',
-      nilaiAgamaBudipekerti: 'SUDAH_BERKEMBANG',
-      deskripsiAgamaBudipekerti:
-        'John adalah siswa yang memiliki pemahaman yang baik tentang nilai-nilai agama dan budipekerti.',
-      nilaiJatiDiri: 'SUDAH_BERKEMBANG',
-      deskripsiJatiDiri:
-        'John telah menunjukkan perkembangan yang positif dalam memahami jati dirinya sebagai individu.',
-      nilaiLiterasiSains: 'SUDAH_BERKEMBANG',
-      deskripsiLiterasiSains:
-        'John memiliki kemampuan literasi sains yang baik dan dapat menerapkan pengetahuannya dalam kehidupan sehari-hari.',
-    },
-    {
-      id: 2,
-      nis: '0987654321',
-      nama: 'Jane Doe',
-      nilaiAgamaBudipekerti: 'MULAI_BERKEMBANG',
-      deskripsiAgamaBudipekerti:
-        'Jane masih dalam tahap perkembangan dalam memahami nilai-nilai agama dan budipekerti.',
-      nilaiJatiDiri: 'BELUM_BERKEMBANG',
-      deskripsiJatiDiri:
-        'Jane perlu bimbingan lebih lanjut untuk memahami jati dirinya sebagai individu.',
-      nilaiLiterasiSains: 'BELUM_BERKEMBANG',
-      deskripsiLiterasiSains:
-        'Jane memerlukan bimbingan ekstra dalam meningkatkan kemampuan literasi sainsnya.',
-    },
-    // Tambahkan data siswa lainnya sesuai kebutuhan
-  ]
+
+  const menu = (record) => (
+    <Menu>
+      <Menu.Item key="edit">Edit Nilai</Menu.Item>
+      <Menu.Item key="delete">Hapus Nilai</Menu.Item>
+      <Menu.Item key="input">Input Nilai</Menu.Item>
+    </Menu>
+  )
 
   const columns = [
     {
       title: 'No',
       dataIndex: 'id',
       key: 'id',
+      render: (text, record, index) => index + 1,
     },
     {
       title: 'NIS',
@@ -119,31 +89,18 @@ const MuridTable = ({ idRombelSemesterGuru, tp }) => {
     {
       title: 'Aksi',
       key: 'action',
-      render: () => (
-        <span>
-          <Button
-            type="primary"
-            icon={<i className="icon fa fa-edit"></i>}
-            style={{ marginRight: '2px', marginLeft: '2px' }}
-          />
-          <Button
-            type="info"
-            icon={<i className="icon fa fa-eye"></i>}
-            style={{ marginRight: '2px', marginLeft: '2px' }}
-          />
-          <Button
-            type="danger"
-            icon={<i className="icon fa fa-trash"></i>}
-            style={{ marginRight: '2px', marginLeft: '2px' }}
-          />
-        </span>
+      render: (text, record) => (
+        <Dropdown overlay={menu(record)} trigger={['click']}>
+          <Button type="primary" icon={<MoreOutlined />} />
+        </Dropdown>
       ),
     },
   ]
   return (
     <Table
       columns={columns}
-      dataSource={dummyData}
+      dataSource={muridWithPenilaian}
+      loading={isFetchingMuridWithPenilaian}
       scroll={{ x: 1000 }}
       bordered
     />
