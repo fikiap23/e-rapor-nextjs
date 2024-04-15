978f82b9f2f29d04dcecc330f7cc18f0089ed852
fikiaprian23 <fikiaprian23@gmail.com>
2024-04-15 13:03:07 +0700

    delete absensi

--- backend/src/absensi/dto/update-absensi.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAbsensiDto } from './create-absensi.dto';
-
-export class UpdateAbsensiDto extends PartialType(CreateAbsensiDto) { }
