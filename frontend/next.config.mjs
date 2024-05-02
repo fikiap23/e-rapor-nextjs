dcdf074b75b1be2d0b7cb5a819132e198788cbee
fikiaprian23 <fikiaprian23@gmail.com>
2024-05-02 23:38:34 +0700

    change server

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'http://103.235.73.130:5000',
+        hostname: 'http://82.180.160.55:5000',
       },
     ],
   },
