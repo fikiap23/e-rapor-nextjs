f83e06aa7bcf346a1a35a1bcb32c9477e9a2aefb
fikiaprian23 <fikiaprian23@gmail.com>
2024-12-16 10:49:48 +0700

    fix

--- backend/src/jadwal-ajar/dto/create-jadwal-ajar.dto.ts
+++ backend/src/jadwal-ajar/dto/create-jadwal-ajar.dto.ts
@@ -1,42 +1,55 @@
-import { IsString, IsDate, IsNotEmpty, IsArray } from 'class-validator';
+import { IsString, IsDate, IsArray, IsOptional, IsNotEmpty, } from 'class-validator';
 export class CreateJadwalAjarDto {
 
+    @IsOptional()
     @IsArray()
     kegiatanIntiHari1: string[];
 
+    @IsOptional()
     @IsArray()
     kegiatanIntiHari2: string[];
 
+    @IsOptional()
     @IsArray()
     kegiatanIntiHari3: string[];
 
+    @IsOptional()
     @IsArray()
     kegiatanIntiHari4: string[];
 
+    @IsOptional()
     @IsArray()
     kegiatanIntiHari5: string[];
 
+    @IsOptional()
     @IsArray()
     kegiatanIntiHari6: string[];
 
+    @IsOptional()
     @IsDate()
     tanggalHari1: Date;
 
+    @IsOptional()
     @IsDate()
     tanggalHari2: Date;
 
+    @IsOptional()
     @IsDate()
     tanggalHari3: Date;
 
+    @IsOptional()
     @IsDate()
     tanggalHari4: Date;
 
+    @IsOptional()
     @IsDate()
     tanggalHari5: Date;
 
+    @IsOptional()
     @IsDate()
     tanggalHari6: Date;
 
+    @IsNotEmpty()
     @IsString()
     idModulAjar: string;
 }
